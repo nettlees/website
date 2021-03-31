f163377e0c5eee3749beb02eb9d14b8867b4a2fe
Nettles <80995666+nettlees@users.noreply.github.com>
2021-03-31 16:08:37 +0100

    Update config.js

--- config.js
+++ config.js
@@ -10,9 +10,9 @@ module.exports = {
   api: 'https://api.devcord.com',
 
   meta: {
-    title: 'devcord',
-    description: 'Devcord is a community that brings together web developers of all experience levels. Get help with your code, connect with other web developers, discuss your current project and chat about your favourite languages, libraries, and frameworks.',
-    thumbnail: '/img/animated-logo-optimized-resized.gif',
+    title: 'ayup',
+    description: 'Ayup is a community that brings together web developers of all experience levels. Get help with your code, connect with other web developers, discuss your current project and chat about your favourite languages, libraries, and frameworks.',
+    thumbnail: 'https://media.discordapp.net/attachments/822823693151502356/826835047054311484/ayup.gif',
     themeColor: '#f0134d',
     url: 'https://www.ayup.ga',
   },
