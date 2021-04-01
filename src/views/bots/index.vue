ecf99d7f331f40e37c254d1192831260f6daa078
Nettles <80995666+nettlees@users.noreply.github.com>
2021-04-01 16:37:53 +0100

    Create index.vue

---
+++ src/views/bots/index.vue
@@ -0,0 +1,24 @@
+
+<template lang="pug">
+  main
+    Markdown {{ index }}
+</template>
+
+<script>
+  import Markdown from '@/components/Markdown'
+  import index from 'resources/bot-index.md'
+  export default {
+    data () {
+      return {
+        index,
+      }
+    },
+    components: {
+      Markdown,
+    },
+  }
+</script>
+
+<style lang="scss" scoped>
+  @use '../../scss/markdown-container.scss';
+</style>
